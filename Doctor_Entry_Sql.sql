af106b73b0f8bf0ae8db831ec3c419d7231bb11c
piyush18184 <44525418+piyush18184@users.noreply.github.com>
2018-10-30 19:21:39 +0530

    Add files via upload

---
+++ Doctor_Entry_Sql.sql
@@ -0,0 +1,20 @@
+INSERT INTO `db`.`doctor_details`
+(`D_Age`,
+`D_ID`,
+`D_Name`,
+`D_PNo`,
+`D_Email`,
+`D_Add`)
+VALUES
+#(
+(45,'D_101','Prakhar',1234567890,'prakhar@yash.com','ABC'),
+(30,'D_102','Shaney',1234567890,'shaney@yash.com','ABC'),
+(55,'D_103','Snighda',1234567890,'snighda@yash.com','ABC'),
+(32,'D_104','Bhaskar',1234567890,'bhaskar@yash.com','ABC'),
+(44,'D_105','Manak',1234567890,'manak@yash.com','ABC'),
+(44,'D_106','Anjali',1234567890,'anjali@yash.com','ABC'),
+(33,'D_107','Neha',1234567890,'neha@yash.com','ABC'),
+(33,'D_108','Sudhir',1234567890,'sudhir@yash.com','ABC'),
+(55,'D_109','Aman',1234567890,'aman@yash.com','ABC'),
+(39,'D_110','Chhavi',1234567890,'chhavi@yash.com','ABC');
+#)
---
+++ Doctor_Proffesional_Details_Entry_Sql.sql
@@ -0,0 +1,29 @@
+INSERT INTO `db`.`doctor_professional_details`
+(`D_Department`,
+`D_DID`,
+`D_OPD_TIME_START`,
+`D_OPD_TIME_END`,
+`D_Type`,
+`D_Specialization`,
+`D_RoomNo`,
+`D_ExtensionNo`)
+VALUES
+#(              
+('Pathology','D_101','08:00:00','10:00:00','Junior Resident','Pursuing MD in Pathology','R_101',12345),
+('Medicine','D_102','08:00:00','10:00:00','Junior Resident','Pursuing MD in Medicine','R_102',12345),
+('Surgery','D_103','09:00:00','12:00:00','Junior Resident','Pursuing MD in Surgery','R_103',12345),
+('Pediatrics','D_104','13:00:00','17:00:00','Junior Resident','Pursuing MD in Pediatrics','R_104',12345),
+('Gynocology','D_105','12:00:00','14:00:00','Senior Resident','MD in Gynocology','R_105',12345),
+('Oncolgy','D_106','13:00:00','15:00:00','Senior Resident','MD in Oncology','R_106',12345),
+('Dentistry','D_107','08:00:00','10:00:00','Senior Resident','MD in Dentistry','R_107',12345),
+('ENT','D_108','08:00:00','10:00:00','Senior Resident','MD in ENT','R_108',12345),
+('Orthopadics','D_109','10:00:00','11:00:00','Specialist','MD in Orthopadics','R_109',12345),
+('Pulmonary Medicine','D_110','15:00:00','18:00:00','Senior Specialist','MD in Pulmonary Medicine','R_110',12345),
+('Neurology','D_111','18:00:00','20:00:00','Specialist','MD in Neurology','R_111',12345),
+('Physiotherapy','D_112','20:00:00','21:00:00','Specialist','MD in Physiotherapy','R_112',12345),
+('Pyschiatry','D_113','19:00:00','21:00:00','Senior Specialist','MD in Pyschiatry','R_113',12345),
+('Radiology','D_114','16:00:00','18:00:00','HOD','MD in Radiology','R_114',12345),
+('Cardiology','D_115','18:00:00','19:00:00','HOD','MD in Cardiology','R_115',12345),
+('Dermatology','D_116','10:00:00','11:00:00','HOD','MD in Dermatology','R_116',12345)
+;
+#)
