48f3e1ad37928ae49a0c07bb14da0c9c77a88f13
vivek18179 <44525385+vivek18179@users.noreply.github.com>
2018-10-30 22:22:24 +0530

    Add files via upload

---
+++ department.py
@@ -0,0 +1,55 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Oct 30 17:10:06 2018
+
+@author: lenovo
+"""
+
+class department:
+    def __init__(self):
+        pass
+
+    
+            
+    def setdepid(self):
+        self.Dep_ID=Dep_ID
+    
+    def setdephod(self):
+        self.Dep._HOD=Dep_HOD
+    
+    def setdep_building(self):
+        self.Dep_building=Dep_building
+    
+    def setdepcontact(self):
+        self.Dep_contact=Dep_contact
+    
+    def setdeprooms(self):
+        self.Dep_rooms=Dep_rooms
+    
+    def setdepopd(self):
+        self.Dep_opd=Dep_opd
+    
+    def setdepname(self):
+        self.Dep_name=Dep_name
+    
+    def getdepname(self):
+        return self.Dep_name
+    
+    def getdepopd(self):
+        return self.Dep_opd
+    
+    def getdepbuilding(self):
+        return self.Dep_building
+    
+    def getdepcontact(self):
+        return self.Dep_contact
+    
+    def getdeprooms(self):
+        return self.Dep_rooms
+    
+    def getdephod(self):
+        return self.Dep_HOD
+    
+    def getdepid(self):
+        return self.Dep_ID
+    
