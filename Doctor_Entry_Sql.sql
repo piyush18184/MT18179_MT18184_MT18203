a1f4b2fbe9bd3e111de0e28c92da822b8fa00759
piyush18184 <44525418+piyush18184@users.noreply.github.com>
2018-10-30 22:56:12 +0530

    Add files via upload

--- Doctor_Entry_Sql.sql
+++ Doctor_Entry_Sql.sql
@@ -27,5 +27,12 @@ VALUES
 (33,'D_107','Neha',1234567890,'neha@yash.com','ABC'),
 (33,'D_108','Sudhir',1234567890,'sudhir@yash.com','ABC'),
 (55,'D_109','Aman',1234567890,'aman@yash.com','ABC'),
-(39,'D_110','Chhavi',1234567890,'chhavi@yash.com','ABC');
+(39,'D_110','Chhavi',1234567890,'chhavi@yash.com','ABC'),
+(45,'D_111','Vinod',1234567890,'vinod@yash.com','ABC'),
+(30,'D_112','Vijay',1234567890,'vijay@yash.com','ABC'),
+(55,'D_113','Viraj',1234567890,'viraj@yash.com','ABC'),
+(32,'D_114','Vineet',1234567890,'vineet@yash.com','ABC'),
+(44,'D_115','Mayank',1234567890,'mayank@yash.com','ABC'),
+(44,'D_116','Shraddha',1234567890,'shraddha@yash.com','ABC')
+;
 #)
