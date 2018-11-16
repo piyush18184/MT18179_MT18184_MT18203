e8bc34a93154fc3fd7fb5c8f40a2840ec96e5897
piyush18184 <44525418+piyush18184@users.noreply.github.com>
2018-11-17 02:17:48 +0530

    Add files via upload

---
+++ opd.sql
@@ -0,0 +1,6 @@
+CREATE TABLE `opd` (
+  `D_ID` varchar(10) NOT NULL,
+  `D_dep` varchar(45) NOT NULL,
+  `D_name` varchar(45) NOT NULL,
+  PRIMARY KEY (`D_dep`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
