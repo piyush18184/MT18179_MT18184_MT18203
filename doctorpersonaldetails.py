e5e842d1cc4b171e70d5d2e16c71839ca119b7aa
jitendraMT18203 <44525565+jitendraMT18203@users.noreply.github.com>
2018-11-20 14:03:47 +0530

    Update doctorpersonaldetails.py

--- doctorpersonaldetails.py
+++ doctorpersonaldetails.py
@@ -3,10 +3,10 @@ import pymysql
 db = pymysql.connect(
     host='127.0.0.1',
     user="root",
-    passwd="",
-    db="db"
+    passwd="6%w<RPl4",
+    db="mydb"
 )
-# print(db)
+# print(mydb)
 cursor = db.cursor()
 
 
@@ -23,54 +23,62 @@ class doctorpersonaldetails:
     def setdoctorname(self, name, did):
         self.name = name
         self.did = did
-        cursor.execute("UPDATE `db`.`doctor_details` SET D_Name = '" + self.name + "' WHERE D_ID = '" + self.did + "';")
+        cursor.execute("UPDATE `mydb`.`doctor_details` SET D_Name = '" + self.name + "' WHERE D_ID = '" + self.did + "';")
         db.commit()
 
     def setdoctorage(self, ageee, did):
         self.age = ageee
         self.did = did
-        cursor.execute("UPDATE `db`.`doctor_details` SET D_Age = '" + self.age + "' WHERE D_ID = '" + self.did + "';")
+        cursor.execute("UPDATE `mydb`.`doctor_details` SET D_Age = '" + self.age + "' WHERE D_ID = '" + self.did + "';")
         db.commit()
 
     def setdoctorpno(self, pno, did):
         self.did = did
         self.pno = pno
-        cursor.execute("UPDATE `db`.`doctor_details` SET D_PNo = '" + self.pno + "' WHERE D_ID = '" + self.did + "';")
+        cursor.execute("UPDATE `mydb`.`doctor_details` SET D_PNo = '" + self.pno + "' WHERE D_ID = '" + self.did + "';")
         db.commit()
 
     def setdoctoremail(self, emaii, did):
         self.did = did
         self.email = emaii
-        cursor.execute("UPDATE `db`.`doctor_details` SET D_Email = '" + self.email + "' WHERE D_ID = '" + self.did + "';")
+        cursor.execute("UPDATE `mydb`.`doctor_details` SET D_Email = '" + self.email + "' WHERE D_ID = '" + self.did + "';")
         db.commit()
 
     def setdoctoraddr(self, addr, did):
         self.addr = addr
         self.did = did
-        cursor.execute("UPDATE `db`.`doctor_details` SET D_Add = '" + self.addr + "' WHERE D_ID = '" + self.did + "';")
+        cursor.execute("UPDATE `mydb`.`doctor_details` SET D_Add = '" + self.addr + "' WHERE D_ID = '" + self.did + "';")
         db.commit()
 
     def getdocdetail(self, did):
         self.did =did
-        cursor.execute("SELECT * FROM `db`.`doctor_details` WHERE D_ID='" + self.did + "';")
+        cursor.execute("SELECT * FROM `mydb`.`doctor_details` WHERE D_ID='" + self.did + "';")
         print(cursor.fetchall())
 
     def getdocname(self, did):
         self.did = did
-        cursor.execute("SELECT D_Name FROM `db`.`doctor_details` WHERE D_ID='" + self.did + "';")
+        cursor.execute("SELECT D_Name FROM `mydb`.`doctor_details` WHERE D_ID='" + self.did + "';")
         print(cursor.fetchone())
 
     def getdocemail(self, did):
         self.did = did
-        cursor.execute("SELECT D_Email FROM `db`.`doctor_details` WHERE D_ID='" + self.did + "';")
+        cursor.execute("SELECT D_Email FROM `mydb`.`doctor_details` WHERE D_ID='" + self.did + "';")
         print(cursor.fetchall())
 
     def getdocphone(self,did):
         self.did = did
-        cursor.execute("SELECT D_PNo FROM `db`.`doctor_details` WHERE D_ID='" + self.did + "';")
+        cursor.execute("SELECT D_PNo FROM `mydb`.`doctor_details` WHERE D_ID='" + self.did + "';")
         print(cursor.fetchall())
 
     def getdocaddress(self,did):
         self.did = did
-        cursor.execute("SELECT D_Add FROM `db`.`doctor_details` WHERE D_ID='" + self.did + "';")
+        cursor.execute("SELECT D_Add FROM `mydb`.`doctor_details` WHERE D_ID='" + self.did + "';")
         print(cursor.fetchall())
+
+
+# a = doctorpersonaldetails()
+# a.getdocaddress('D_105')
+# a.getdocdetail('D_105')
+# a.getdocname('D_105')
+# a.getdocphone('D_105')
+# a.getdocemail('D_105')
