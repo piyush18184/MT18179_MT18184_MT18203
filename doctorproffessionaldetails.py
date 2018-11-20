338ce28f6cf5e2a20bb63e2fb5151aac3f9b998e
jitendraMT18203 <44525565+jitendraMT18203@users.noreply.github.com>
2018-11-20 13:57:26 +0530

    Update doctorproffessionaldetails.py

--- doctorproffessionaldetails.py
+++ doctorproffessionaldetails.py
@@ -4,7 +4,7 @@ db = pymysql.connect(
     host='127.0.0.1',
     user="root",
     passwd="6%w<RPl4",
-    db="db"
+    db="mydb"
 )
 print(db)
 cursor = db.cursor()
@@ -26,123 +26,130 @@ class doctorprofessionaldetails:
         self.name = name
     def getdocprofessinalall(self, did):
         self.did = did
-        cursor.execute("SELECT * FROM `db`.`doctor_professional_details` WHERE D_ID='" + self.did + "';")
+        cursor.execute("SELECT * FROM `mydb`.`doctor_professional_details` WHERE D_DID='" + self.did + "';")
         print(cursor.fetchall())
 
     def setdoctordepartment(self, dep, did):
         self.dep = dep
         self.did = did
         cursor.execute(
-            "UPDATE `db`.`doctor_professional_details` SET D_Department = '" + self.dep + "' WHERE D_ID = '" + self.did + "';")
+            "UPDATE `mydb`.`doctor_professional_details` SET D_Department = '" + self.dep + "' WHERE D_ID = '" + self.did + "';")
         db.commit()
 
     def assigndoctoropd(self, did):
         self.did = did
-        cursor.execute("SELECT D_Department FROM `db`.`doctor_professional_details` WHERE D_DID = '" + self.did + "';")
+        cursor.execute("SELECT D_Department FROM `mydb`.`doctor_professional_details` WHERE D_DID = '" + self.did + "';")
         aa=cursor.fetchone()
         bb=aa[0]
         self.dep = bb
-        cursor.execute("SELECT D_Name FROM `db`.`doctor_details` WHERE D_ID = '" + self.did + "';")
+        cursor.execute("SELECT D_Name FROM `mydb`.`doctor_details` WHERE D_ID = '" + self.did + "';")
         cc = cursor.fetchone()
         dd = cc[0]
         self.name = dd
-        cursor.execute("SELECT count(D_ID) FROM `db`.`opd` WHERE D_dep='"+self.dep+"';")
+        cursor.execute("SELECT count(D_ID) FROM `mydb`.`opd` WHERE D_dep='"+self.dep+"';")
         xxx =cursor.fetchone()
         yyy=int(xxx[0])
         if (yyy==0):
-            sql="INSERT INTO `db`.`opd` (`D_ID`,`D_dep`,`D_name`) VALUES('%s','%s','%s')"
+            sql="INSERT INTO `mydb`.`opd` (`D_ID`,`D_dep`,`D_name`) VALUES('%s','%s','%s')"
             val=(self.did,self.dep,self.name)
             cursor.execute(sql % val)
             db.commit()
         else:
-            cursor.execute("UPDATE `db`.`opd` SET D_ID = '" + self.did + "' , D_dep='" + self.dep + "' , D_name='" + self.name + "' WHERE D_dep = '" + bb + "';")
+            cursor.execute("UPDATE `mydb`.`opd` SET D_ID = '" + self.did + "' , D_dep='" + self.dep + "' , D_name='" + self.name + "' WHERE D_dep = '" + bb + "';")
             db.commit()
 
     def setopdstarttime(self, opdst, did):
         self.opdst = opdst
         self.did = did
         cursor.execute(
-            "UPDATE `db`.`doctor_professional_details` SET D_OPD_TIME_START = '" + self.opdst + "' WHERE D_DID = '" + self.did + "';")
+            "UPDATE `mydb`.`doctor_professional_details` SET D_OPD_TIME_START = '" + self.opdst + "' WHERE D_DID = '" + self.did + "';")
         db.commit()
 
     def setopdendttime(self, opdet, did):
         self.opdet = opdet
         self.did = did
         cursor.execute(
-            "UPDATE `db`.`doctor_professional_details` SET D_OPD_TIME_END = '" + self.opdet + "' WHERE D_DID = '" + self.did + "';")
+            "UPDATE `mydb`.`doctor_professional_details` SET D_OPD_TIME_END = '" + self.opdet + "' WHERE D_DID = '" + self.did + "';")
         db.commit()
 
     def setdoctype(self, dtyp, did):
         self.dtyp = dtyp
         self.did = did
         cursor.execute(
-            "UPDATE `db`.`doctor_professional_details` SET D_Type = '" + self.dtyp + "' WHERE D_DID = '" + self.did + "';")
+            "UPDATE `mydb`.`doctor_professional_details` SET D_Type = '" + self.dtyp + "' WHERE D_DID = '" + self.did + "';")
         db.commit()
     def getdoctype(self, did):
         self.did = did
         cursor.execute(
-            "SELECT D_Type FROM `db`.`doctor_professional_details` WHERE D_DID = '" + self.did + "';")
+            "SELECT D_Type FROM `mydb`.`doctor_professional_details` WHERE D_DID = '" + self.did + "';")
         print(cursor.fetchone())
     def setdocspecialisation(self, spec, did):
         self.spec = spec
         self.did = did
         cursor.execute(
-            "UPDATE `db`.`doctor_professional_details` SET D_Specialization = '" + self.spec + "' WHERE D_ID = '" + self.did + "';")
+            "UPDATE `mydb`.`doctor_professional_details` SET D_Specialization = '" + self.spec + "' WHERE D_DID = '" + self.did + "';")
         db.commit()
     def getdocspecialisation(self,did):
         self.did = did
         cursor.execute(
-            "SELECT D_Specialization FROM `db`.`doctor_professional_details` WHERE D_ID = '" + self.did + "';")
+            "SELECT D_Specialization FROM `mydb`.`doctor_professional_details` WHERE D_DID = '" + self.did + "';")
         print(cursor.fetchone())
     def setextensionno(self, ext, did):
         self.extno = ext
         self.did = did
         cursor.execute(
-            "UPDATE `db`.`doctor_professional_details` SET D_ExtensionNo = '" + self.extno + "' WHERE D_ID = '" + self.did + "';")
+            "UPDATE `mydb`.`doctor_professional_details` SET D_ExtensionNo = '" + self.extno + "' WHERE D_DID = '" + self.did + "';")
         db.commit()
 
     def setroomno(self, rmno, did):
         self.rmno = rmno
         self.did = did
         cursor.execute(
-            "UPDATE `db`.`doctor_professional_details` SET D_RoomNo = '" + self.rmno + "' WHERE D_ID = '" + self.did + "';")
+            "UPDATE `mydb`.`doctor_professional_details` SET D_RoomNo = '" + self.rmno + "' WHERE D_DID = '" + self.did + "';")
         db.commit()
     def getdoctype(self, did):
         self.did = did
         cursor.execute(
-            "SELECT D_Type FROM `db`.`doctor_professional_details` WHERE D_DID = '" + self.did + "';")
+            "SELECT D_Type FROM `mydb`.`doctor_professional_details` WHERE D_DID = '" + self.did + "';")
         print(cursor.fetchone())
     def getopdstarttime(self, did):
          self.did = did
          cursor.execute(
-            "SELECT D_OPD_TIME_START FROM `db`.`doctor_professional_details` WHERE D_DID = '" + self.did + "';")
+            "SELECT D_OPD_TIME_START FROM `mydb`.`doctor_professional_details` WHERE D_DID = '" + self.did + "';")
          print(cursor.fetchone())
     def getopdendtime(self, did):
          self.did = did
          cursor.execute(
-            "SELECT D_OPD_TIME_END FROM `db`.`doctor_professional_details` WHERE D_DID = '" + self.did + "';")
+            "SELECT D_OPD_TIME_END FROM `mydb`.`doctor_professional_details` WHERE D_DID = '" + self.did + "';")
          print(cursor.fetchone())
     def getroomno(self,did):
         self.did = did
         cursor.execute(
-            "SELECT D_RoomNo FROM `db`.`doctor_professional_details` WHERE D_ID = '" + self.did + "';")
+            "SELECT D_RoomNo FROM `mydb`.`doctor_professional_details` WHERE D_DID = '" + self.did + "';")
         print(cursor.fetchone())
     def getextensionno(self,did):
         self.did = did
         cursor.execute(
-            "SELECT D_ExtensionNo FROM `db`.`doctor_professional_details` WHERE D_ID = '" + self.did + "';")
+            "SELECT D_ExtensionNo FROM `mydb`.`doctor_professional_details` WHERE D_DID = '" + self.did + "';")
         print(cursor.fetchone())
 
     def getdoctordepartment(self,did):
 
         self.did = did
         cursor.execute(
-            "SELECT D_Department FROM `db`.`doctor_professional_details` WHERE D_ID = '" + self.did + "';")
+            "SELECT D_Department FROM `mydb`.`doctor_professional_details` WHERE D_DID = '" + self.did + "';")
         print(cursor.fetchone())
 
 
 
 
 
-a = doctorprofessionaldetails()
-a.getdocprofessinalall('D_101')
+# a = doctorprofessionaldetails()
+# a.getdocprofessinalall('D_101')
+# a.getdocspecialisation('D_101')
+# a.getdoctordepartment('D_101')
+# a.getdoctype('D_101')
+# a.getopdstarttime('D_101')
+# a.getopdendtime('D_101')
+# a.getextensionno('D_101')
+# a.getroomno('D_101')
